245474a2b42d9a5642b0310870ba2a3c40497909
anthonyalcrz <alcarazecommerce@gmail.com>
2025-05-01 14:09:39 -0700

    trying my best

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,11 +1,6 @@
-'use client';
+import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs";
+import type { Database } from "@/types/supabase";
 
-import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
-import type { Database } from '@/types/supabase';
-
-const supabase = createPagesBrowserClient<Database>({
-  supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-});
+const supabase = createPagesBrowserClient<Database>();
 
 export default supabase;
