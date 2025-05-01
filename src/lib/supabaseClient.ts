ca1f0e75a75b407dc840632723a28490e28dbd8b
anthonyalcrz <alcarazecommerce@gmail.com>
2025-05-01 12:34:38 -0700

    updated

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,11 @@
-// src/lib/supabaseClient.ts
-import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
+'use client';
 
-const supabase = createPagesBrowserClient();
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
+import type { Database } from '@/types/supabase';
+
+const supabase = createPagesBrowserClient<Database>({
+  supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+});
 
 export default supabase;
